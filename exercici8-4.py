be9d61d62643d73a2896bae6ff4ba090b6574b52
carsek <93370804+carsek@users.noreply.github.com>
2022-01-10 10:12:29 +0100

    Add files via upload

---
+++ exercici8-4.py
@@ -0,0 +1,7 @@
+print ("Aquest programa demana 7 números i te dona la suma d'aquests 7 ")
+total = 0 
+
+for i in range(7):
+        x = input("Introdueix un número")
+        total = total + float
+    
