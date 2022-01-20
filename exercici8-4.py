1229109dbe733e7aeb0dcd61794d03f5b66f72be
carsek <93370804+carsek@users.noreply.github.com>
2022-01-20 11:59:39 +0100

    Add files via upload

--- exercici8-4.py
+++ exercici8-4.py
@@ -1,16 +1,19 @@
 print ("Aquest programa demana 7 números i te dona la suma d'aquests 7 ")
-total = 0 
+total=0 
+negatiu=0
+positiu=0
+zero=0
 
 for i in range(7):
-        x = input("Introdueix un número")
-        total = total + float(x)
-print("El total és:", total)
-"""
-if x > 0:
-        print("El total es:", total)
-elif x < 0:
-        print("El total es:", total)
-elif x== 0:
-        print("El total es:", total)
-""" 
+        x = float(input("Introdueix un número"))
+        if x > 0:
+                positiu+=1
+        elif x < 0:
+                negatiu-=1
+        else:
+                zero+=1                
+        total = total + x
+print ("El total és:", total)
+print ("Hi ha", positiu,"positius, hi ha", negatiu,"negatius hi ha", zero,"zeros")
+
     
